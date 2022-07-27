e14c5794d619a4e6d49f2077e8e4d372d90e3e75
xxibcill <cchayanin@hotmail.com>
2022-07-27 23:58:10 +0700

    add isPrime

--- lib/Utils.ex
+++ lib/Utils.ex
@@ -7,4 +7,21 @@ defmodule Utils do
       _ -> fibo(n-2) + fibo(n-1)
     end
   end
+
+  # not done yet
+  def fiboSeq(n,count \\ 0,seq \\ [1,1]) when n >= 3 do
+    case count do
+      ^n -> seq
+      _ -> fiboSeq(n,count + 1,[seq | Enum.at(seq, count-1) + Enum.at(seq, count)])
+    end
+  end
+
+  # check if number is prime
+  defp isPrime(n) when n in [2, 3], do: true
+  defp isPrime(n) do
+    floored_sqrt = :math.sqrt(n)
+      |> Float.floor
+      |> round
+    !Enum.any?(2..floored_sqrt, &(rem(n, &1) == 0))
+  end
 end
